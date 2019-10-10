8afd935bcf2c9940209ac84292d863e1e299e2be
Douglas Naphas <douglasnaphas@gmail.com>
2019-10-09 22:40:36 -0400

    Add make-branches script

---
+++ make-branches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+n=$1
+echo "making ${n} branches at different refs"
+max_f_num=$(ls f* | sort | tail -1 | sed 's/^f//')
+for i in {1..${n}}
+do
+  f="f$((max_f_num + i))"
+  git checkout -b ${f}
+  echo "${f}" > ${f}
+  git add ${f}
+  git commit -m "Add ${f}"
+  git push origin --set-upstream origin ${f}
+done
